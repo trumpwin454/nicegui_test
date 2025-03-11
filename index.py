be985979405dbc7788322d021f8d9ea093c1bc94
trumpwin454 <154766305+trumpwin454@users.noreply.github.com>
2025-03-10 20:34:56 -0700

    fix bug

--- index.py
+++ index.py
@@ -1,19 +1,34 @@
-from fastapi import FastAPI
+#!/usr/bin/env python3
+from router import Router
 
-from nicegui import app, ui
+from nicegui import ui
 
 
-def init(fastapi_app: FastAPI) -> None:
-    @ui.page('/')
-    def show():
-        ui.label('Hello, FastAPI!')
+@ui.page('/')  # normal index page (e.g. the entry point of the app)
+@ui.page('/{_:path}')  # all other pages will be handled by the router but must be registered to also show the SPA index page
+def main():
+    router = Router()
 
-        # NOTE dark mode will be persistent for each user across tabs and server restarts
-        ui.dark_mode().bind_value(app.storage.user, 'dark_mode')
-        ui.checkbox('dark mode').bind_value(app.storage.user, 'dark_mode')
+    @router.add('/')
+    def show_one():
+        ui.label('Content One').classes('text-2xl')
 
-    ui.run_with(
-        fastapi_app,
-        mount_path='/gui',  # NOTE this can be omitted if you want the paths passed to @ui.page to be at the root
-        storage_secret='pick your private secret here',  # NOTE setting a secret is optional but allows for persistent storage per user
-    )
+    @router.add('/two')
+    def show_two():
+        ui.label('Content Two').classes('text-2xl')
+
+    @router.add('/three')
+    def show_three():
+        ui.label('Content Three').classes('text-2xl')
+
+    # adding some navigation buttons to switch between the different pages
+    with ui.row():
+        ui.button('One', on_click=lambda: router.open(show_one)).classes('w-32')
+        ui.button('Two', on_click=lambda: router.open(show_two)).classes('w-32')
+        ui.button('Three', on_click=lambda: router.open(show_three)).classes('w-32')
+
+    # this places the content which should be displayed
+    router.frame().classes('w-full p-4 bg-gray-100')
+
+
+ui.run()
